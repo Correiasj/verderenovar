01a400c99a5565b847612e6347e8a900d1b713f6
Correiasj <meugato556@gmail.com>
2024-05-02 10:36:19 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Rota para a página inicial
+@app.route('/')
+def index():
+    return render_template("home.html")
+
+# Rota que renderiza uma página HTML
+@app.route('/sobre')
+def pagina_html():
+    return render_template('sobre.html')
+
+# Rota com variável na URL
+@app.route('/usuario/<nome>')
+def usuario(nome):
+    return render_template('usuario.html', nome=nome)
+
+if __name__ == '__main__':
+    app.run(debug=True)
